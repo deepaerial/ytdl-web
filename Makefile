cd3fc374fea5b80d418b3c449d00b7da68329bd0
deepaerial <nazarii.oleksiuk@gmail.com>
2020-06-27 14:33:18 +0200

    Adding frontend

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ clean:
 	@find . -type f -name "*.py[co]" -delete -or -type d -name "__pycache__" -delete
 	@find . -type d -name "*pytest_cache*" -exec rm -rf {} +
 
-dev:
+run:
 	@poetry run python server.py
 
 test:
---
+++ ytdl_web/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>This is a test designed to provoke an emotional response...</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
