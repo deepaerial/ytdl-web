a5b16db669cade132996ab05be53f9f598bbc873
Deep Aerial <32838295+deepaerial@users.noreply.github.com>
2023-04-18 09:11:53 +0200

    Prepare app for deploy (#4)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
   server: {
     port: 8080,
     strictPort: true,
     host: true
+  },
+  build: {
+    rollupOptions: {
+      output:
+      {
+        format: 'es',
+        strict: false,
+        entryFileNames: "[name].js",
+        dir: 'dist/'
+      }
+    }
   }
 });
